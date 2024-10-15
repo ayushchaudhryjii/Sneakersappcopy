0cbec75403d8442fb47bc266eb039f3c7a84de61
akash sharma <ayushchaudhary03032002@gmail.com>
2024-10-14 21:55:40 -0700

    14oct

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import AppNavigator from "./src/navigator/AppNavigator";
+import store from "./src/redux/Store";
+import { Provider } from "react-redux";
 
 export default function App() {
-  return <AppNavigator />
+
+  return (
+    
+    <Provider store={store}>
+    <AppNavigator />
+      </Provider>
+    
+  
+
+  )
+
+  
 }
